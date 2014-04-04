98af823a7c1b14d90e7c209f308a3098e346cc4b
othtim <othtim@gmail.com>
2014-04-03 20:09:27 -0600

    thread code and grab site for viewstate
    Implemented threading. Creates LWP object per thread. Requests the start
    page (to get the viewstate and hidden fields required for submitting)

--- main.pl
+++ main.pl
@@ -1,18 +1,32 @@
 
+#use strict;
+#use warnings;
 use Time::HiRes qw(gettimeofday);
 use Data::Dumper qw(Dumper);
+use LWP::UserAgent;
+use HTTP::Request::Common qw(GET);
+use HTTP::Cookies;
 
-$DEBUG = 1;
+
+
+$DEBUG = 0;
 $SIG{'INT'} = \&exit_clean;
 $SIG{'QUIT'} = \&exit_clean;
 $SIG{'CHLD'} = \&child_handler;
 
-# data structure
-# contains an id ($id)
-# $data{1}{time} = 
-#
+#cookie info
+ $cookie_jar = HTTP::Cookies->new(
+    file => "C:\lwp_cookies.dat",
+    autosave => 1,
+  );
 
+
+# data structure
 my %data;
+
+
+# where to point the script -change later
+$target = 'https://schoollogic.psd70.ab.ca/Schoollogic/';
 
 
 my $id;
@@ -21,20 +35,50 @@ my $maxThreads = 2;
 #worker loop
 for($id = 1;$id <= $maxThreads; $id++){
 
+	#initial timer
 	$data{$id}{time} = Time::HiRes::gettimeofday();
 
-	
 	#child process time
 	$newprocess = fork();
 	if($newprocess == 0){
 	
-		print $data{$id}{time};
+		#create a new session. $data{$id}{browser} is the main object
+		$data{$id}{browser} = LWP::UserAgent->new();
+		
+		#create a user-agent based on the thread id
+		$data{$id}{browser}->agent('Tim'.$id);
+		
+		#store cookie jar in memory in this threads hash
+		$data{$id}{browser}->cookie_jar( $data{$id}{cookie_jar} );
+		
+		
+		#we need to make an inital request to get the viewstate
+		{
+			#assemble request
+			$data{$id}{request} = GET $target;
+			
+			#make request
+			$data{$id}{response} = $data{$id}{browser}->request( $data{$id}{request} );
+		
+			#check that things worked
+			if ($data{$id}{response}->is_success) {
+				print $data{$id}{response}->content;
+			} else {
+				print $data{$id}{response}->status_line . "\n";
+			}
+			
+		}
+			
+	
+		#get time elapsed since thread started
+		$data{$id}{time} = Time::HiRes::gettimeofday() - $data{$id}{time};
 		exit(0);
 	}
 
-	}
+}
 
-	
+
+
 
 #check work
 if($DEBUG){
@@ -45,7 +89,6 @@ if($DEBUG){
 		}
 	}
 }
-
 
 
 #########subs
