f86ce544767926cd6ff98c399c9046c146d88d3f
othtim <othtim@gmail.com>
2014-04-05 00:56:32 -0600

    log file implemented
    log file implemented

--- main.pl
+++ main.pl
@@ -1,16 +1,20 @@
+#example: perl mail.pl -u userlist.txt -t logfile.txt
 #
-#
-#
+#todo: make this thing count the number of threads that are active, and keep that number up. and stay alive until control-c
+# 
+
+
 
 #use strict;
 #use warnings;
+use Cwd;
 use Time::HiRes qw(gettimeofday);
 use Data::Dumper qw(Dumper);
 use HTTP::Request::Common qw(GET);
 use HTTP::Cookies;
 use WWW::Mechanize;
 
-$DEBUG = 0;
+my $DEBUG = 0;
 $SIG{'INT'} = \&exit_clean;
 $SIG{'QUIT'} = \&exit_clean;
 $SIG{'CHLD'} = \&child_handler;
@@ -18,14 +22,30 @@ $SIG{'CHLD'} = \&child_handler;
 my $userlist_index_counter; #counter for incrementing the index when populating %userlist hash
 my $userlist_inuse_LOCK = 0; #variable to store a lock value for the "inuse" check.
 
+my $log_file;
+my $log_filehandle;
 
 ##parse arguments
-OUTER:
-for ($argv_ident_counter = 0 ; $argv_ident_counter < @ARGV ; $argv_ident_counter++){  
-  if (@ARGV[$argv_ident_counter] eq "-u"){
-      $userlist = @ARGV[$argv_ident_counter + 1];
-  }
+for (my $argv_ident_counter = 0 ; $argv_ident_counter < @ARGV ; $argv_ident_counter++){  
+	#skip the last argument, it wont be a switch
+	next if($argv_ident_counter == $#ARGV );
+	if ($ARGV[$argv_ident_counter] eq "-u"){
+		$userlist_file = $ARGV[$argv_ident_counter + 1];
+	}elsif($ARGV[$argv_ident_counter] eq "-t"){
+		$log_file = $ARGV[$argv_ident_counter + 1];
+	}	
 }
+	
+
+##open logfile if exists, otherwise no logging.
+if(defined $log_file){
+	#$log_file = $curdir;
+	open($log_filehandle , ">>" , $log_file) || die("could not find user list $log_file, quitting...\n");
+	#and we just keep this open to write to. via log_message(). close at end of program run.
+}
+
+
+
 
 ##parse userlist
 # $users{someGeneratedInt}{username} = 'joe';	#un for this user
@@ -34,9 +54,9 @@ for ($argv_ident_counter = 0 ; $argv_ident_counter < @ARGV ; $argv_ident_counter
 my %userlist;
 
 #open user list file for reading
-open(USERLIST , $userlist) || die("could not find user list $userlist, quitting...\n");
+open(my $userlist_filehandle , "<", $userlist_file) || die("could not find user list $userlist, quitting...\n");
 $userlist_index_counter = 1;
-while(<USERLIST>){
+while(<$userlist_filehandle>){
 	#if the format of the record is okay, load into data structure
 	if( m/^(\s|\S)+,(\S|\s)*$/ ){
 		$userlist{$userlist_index_counter}{username} = $1;
@@ -45,7 +65,9 @@ while(<USERLIST>){
 		$userlist_index_counter++;
 	}
 }
-close(USERLIST) || warn("could not close user list $userlist \n");
+close($userlist_filehandle) || warn("could not close user list $userlist \n");
+
+
 
 
 # data structure
@@ -65,110 +87,135 @@ $target = 'https://schoollogic.psd70.ab.ca/SchoolLogic/login.aspx?ReturnUrl=%2fS
 my $id;
 my $maxThreads = 1;
 
-#worker loop
-for($id = 1;$id <= $maxThreads; $id++){
 
-	#initial timer for timing the thread
-	$data{$id}{time} = Time::HiRes::gettimeofday();
 
-	#child process time
-	$newprocess = fork();
-	if($newprocess == 0){
-	
-		#create a new session. $data{$id}{browser} is the main object
-		#store cookie jar in memory in this threads hash
-		$data{$id}{browser} = WWW::Mechanize->new( cookie_jar => {} );
+#while(1){
+
+	#worker loop
+	for($id = 1;$id <= $maxThreads; $id++){
+
+		#initial timer for timing the thread
+		$data{$id}{time} = Time::HiRes::gettimeofday();
+
+		#child process time
+		$newprocess = fork();
+		if($newprocess == 0){
 		
-		#create a user-agent based on the thread id
-		$data{$id}{browser}->agent('Mozilla/5.0'.$id);
+
 		
-		#we need to search through %userlist. this could create a race condition, so lock this function.
-		#this should be safe enough.
-		#loop until lock is free
-		USERLIST_INUSE_LOCK:
-		while($userlist_inuse_LOCK == 1){
+			#create a new session. $data{$id}{browser} is the main object
+			#store cookie jar in memory in this threads hash
+			$data{$id}{browser} = WWW::Mechanize->new( cookie_jar => {} );
 			
-			#if we are here that means this has become unlocked. so relock it.
-			$userlist_inuse_LOCK = 1;
-						
-			foreach my $userlist_sort_indexes (sort keys %userlist) {
-				#and find a user/pw combo that isnt in use
-				if($userlist{$userlist_sort_indexes}{inuse} == 0){
-					#then we are going to take this user and mark it in-use
-					$userlist{$userlist_sort_indexes}{inuse} = 1;
-					#and assign this thread to use this un/pw
-					$data{$id}{username} = $userlist{$userlist_sort_indexes}{username};
-					$data{$id}{password} = $userlist{$userlist_sort_indexes}{password};
-					$data{$id}{unpwindex} = $userlist_sort_indexes;
-				};
-			}
-
-			#we are done now, unlock.
-			$userlist_inuse_LOCK = 0;			
-		}
-		
-		
-		
-		#we make an initial request. login stage
-		{
-			#make request
-			$data{$id}{browser}->get( $target ); 
+			#create a user-agent based on the thread id
+			$data{$id}{browser}->agent('Mozilla/5.0'.$id);
 			
-			#check that things worked
-			if (! $data{$id}{browser}->success()) {
-				print $data{$id}{browser}->status() . "\n"; 
+			#we need to search through %userlist. this could create a race condition, so lock this function.
+			#this should be safe enough.
+			#loop until lock is free
+			USERLIST_INUSE_LOCK:
+			while($userlist_inuse_LOCK == 1){
+				
+				#if we are here that means this has become unlocked. so relock it.
+				$userlist_inuse_LOCK = 1;
+							
+				foreach my $userlist_sort_indexes (sort keys %userlist) {
+					#and find a user/pw combo that isnt in use
+					if($userlist{$userlist_sort_indexes}{inuse} == 0){
+						#then we are going to take this user and mark it in-use
+						$userlist{$userlist_sort_indexes}{inuse} = 1;
+						#and assign this thread to use this un/pw
+						$data{$id}{username} = $userlist{$userlist_sort_indexes}{username};
+						$data{$id}{password} = $userlist{$userlist_sort_indexes}{password};
+						$data{$id}{unpwindex} = $userlist_sort_indexes;
+					};
+				}
+
+				#we are done now, unlock.
+				$userlist_inuse_LOCK = 0;			
 			}
 			
-			#now we login. the field() function sets the value of a field (www::mechanize magic). 
-			#populate username and password, hidden fields, then submit
-			$result = $data{$id}{browser}->submit_form(	form_name => 'Form1',
-														fields => {
-															txtUserName => 'kGarner',
-															txtPassword => '10016',
-															__EVENTVALIDATION => $data{$id}{browser}->value(__EVENTVALIDATION),
-															__VIEWSTATE => $data{$id}{browser}->value(__VIEWSTATE),
-															Submit => 'Login',
-															},
-														button => 'Submit'
-														);
 			
-			#print $result->content();
+			##LOGIN
+			#we make an initial request. login stage
+			{
+				#make request
+				$data{$id}{browser}->get( $target ); 
+				
+				#check that things worked
+				if (! $data{$id}{browser}->success()) {
+					print $data{$id}{browser}->status() . "\n"; 
+				}
+				
+				
+				
+				#now we login. the field() function sets the value of a field (www::mechanize magic). 
+				#populate username and password, hidden fields, then submit
+				$result = $data{$id}{browser}->submit_form(	form_name => 'Form1',
+															fields => {
+																txtUserName => 'kGarner',
+																txtPassword => '10016',
+																__EVENTVALIDATION => $data{$id}{browser}->value(__EVENTVALIDATION),
+																__VIEWSTATE => $data{$id}{browser}->value(__VIEWSTATE),
+																Submit => 'Login',
+																},
+															button => 'Submit'
+															);
+				
+				#print $result->content();
+			
+			}
+			
+			
+			
+			
+			
+			#NEED TO TURN THIS SECTION IN FETCHING JOBS FROM THE WORK LIST
+			#{
+			#
+			#$data{$id}{browser}->get( 'https://schoollogic.psd70.ab.ca/Schoollogic/default.aspx' ); 
+			#print $data{$id}{browser} -> content();
+			#
+			#}
+			
+
+			
+			
+			########################################
+			#IMPLEMENT LOGOUT HERE
+			########################################
+			
+			
+			
+			
+			########################################THREAD CLEANUP
+			#release the username/password
+			$userlist{ $data{$id}{unpwindex} }{inuse} = 0;
+			$data{$id}{username} = '';
+			$data{$id}{password} = '';
+			$data{$id}{unpwindex} = '';
 		
+			#get time elapsed since thread started
+			$data{$id}{time} = Time::HiRes::gettimeofday() - $data{$id}{time};
+			
+			exit(0);
 		}
-		
-		
-		
-		#NEED TO TURN THIS SECTION IN FETCHING JOBS FROM THE WORK LIST
-		#{
-		#
-		#$data{$id}{browser}->get( 'https://schoollogic.psd70.ab.ca/Schoollogic/default.aspx' ); 
-		#print $data{$id}{browser} -> content();
-		#
-		#}
-		
-		
-		
-		
-		
-		########################################
-		#IMPLEMENT LOGOUT HERE
-		########################################
-		
-		
-		########################################THREAD CLEANUP
-		#release the username/password
-		$userlist{ $data{$id}{unpwindex} }{inuse} = 0;
-		$data{$id}{username} = '';
-		$data{$id}{password} = '';
-		$data{$id}{unpwindex} = '';
-	
-		#get time elapsed since thread started
-		$data{$id}{time} = Time::HiRes::gettimeofday() - $data{$id}{time};
-		
-		exit(0);
+
+		sleep(10);
 	}
 
-}
+#end of while() to wait for child functions
+#}
+
+############################################################
+############################################################
+############################################################
+############################################################
+#PROGRAM CLEANUP
+
+
+#close logfile if it exists
+
 
 
 
@@ -183,6 +230,7 @@ if($DEBUG){
 }
 
 
+
 ################################################################
 ################################################################
 ################################################################
@@ -193,14 +241,15 @@ if($DEBUG){
 
 
 sub exit_clean(){
-
-	close(ERRORLOG);
+	#close logfile if it exists
+	if($log_file != 0){ 
+		close($log_filehandle) || warn("could not close log file $log_file \n"); 
+	}
 	$sig_got = @_;
 	$SIG{'INT'}  = 'IGNORE';
 	$SIG{'QUIT'} = 'IGNORE';
 	close(SERVERSOCK);
 	close(CHLDSOCK);
-	if ($errorlog) { print ERRORLOG "Quitting on signal $sig_got"};
 	die("Quitting on signal $sig_got\n");
 }
 
@@ -210,10 +259,30 @@ sub child_handler(){
 }
 
 
-sub parse_users(){
 
-
+#sub that holds open the logfile so that we don't have overhead of opening and closing logfile for each message
+sub log_message(){
+	#if logging to file is enabled do so, otherwise log to stdout
+	if(defined $log_file){
+		#if this isnt true then the logfile has closed unexpectedly
+		if( tell($log_filehandle) != -1 ){
+			#while anything remains in the argument list, remove it from the list and print it to LOGFILE.
+			while(@_){ 
+				print $log_filehandle shift(@_) . "\n"; 
+			}				
+		}
+		else{
+			die("log file closed unexpectedly");
+		}
+	}
+	else{
+		while(@_){ 
+			print shift(@_) . "\n"; 
+		}
+	}
 }
+
+
 
 # for reference
 #
@@ -222,3 +291,16 @@ sub parse_users(){
 #print "$name, $subject: $grades{$name}{$subject}\n";
 #}
 #}
+
+
+
+
+
+
+
+
+
+
+
+
+
