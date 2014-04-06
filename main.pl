d3ebdac62057c8b13b935582a2c115e82360dfe2
othtim <othtim@gmail.com>
2014-04-06 00:20:19 -0600

    changed threading to proper threads module instead of fork()
    changed to proper threads module instead of fork() based. created
    function with semaphore to manage un/pw selection

--- main.pl
+++ main.pl
@@ -1,13 +1,17 @@
 #example: perl mail.pl -u userlist.txt -t logfile.txt
 #
-#todo: make this thing count the number of threads that are active, and keep that number up. and stay alive until control-c
-# also needs to spawn threads at an interval (will hack with sleep() calls until i get the action interval set up))
+# need to add a routine to the parent thread that checks if threads exited abnormally, and regain their un/pw
+# 
+# need to start work loop
 #
 
 
 #use strict;
 #use warnings;
 #use Cwd;
+use threads;
+use threads::shared;
+use Thread::Semaphore;
 use Time::HiRes qw(gettimeofday);
 use Data::Dumper qw(Dumper);
 use HTTP::Request::Common qw(GET);
@@ -19,11 +23,17 @@ $SIG{'INT'} = \&exit_clean;
 $SIG{'QUIT'} = \&exit_clean;
 $SIG{'CHLD'} = \&child_handler;
 
+#for locking use_credentials
+my $use_credentials_semaphore = Thread::Semaphore->new();
+
+
 my $userlist_index_counter; #counter for incrementing the index when populating %userlist hash
-my $userlist_inuse_LOCK = 0; #variable to store a lock value for the "inuse" check.
+my $userlist_inuse_LOCK; #variable to store a lock value for the "inuse" check.
+$userlist_inuse_LOCK = 0;
 
 my $log_file;
 my $log_filehandle;
+
 
 ##parse arguments
 for (my $argv_ident_counter = 0 ; $argv_ident_counter < $#ARGV + 1 ; $argv_ident_counter++){  
@@ -36,14 +46,12 @@ for (my $argv_ident_counter = 0 ; $argv_ident_counter < $#ARGV + 1 ; $argv_ident
 	}	
 }
 	
-
 ##open logfile if exists, otherwise no logging.
 if(defined $log_file){
 	#$log_file = $curdir;
 	open($log_filehandle , ">>" , $log_file) || die("could not find user list $log_file, quitting...\n");
 	#and we just keep this open to write to. via log_message(). close at end of program run.
 }
-
 
 
 ##parse userlist
@@ -55,6 +63,7 @@ my %userlist;
 #open user list file for reading
 open(my $userlist_filehandle , "<", $userlist_file) || die("could not find user list $userlist, quitting...\n");
 $userlist_index_counter = 1;
+
 while(<$userlist_filehandle>){
 	#if the format of the record is okay, load into data structure
 	if( m/^([A-Za-z0-9 ]+),([A-Za-z0-9 ]*)$/ ){
@@ -70,12 +79,16 @@ close($userlist_filehandle) || warn("could not close user list $userlist \n");
 
 # data structure
 # $data{$id}{...}			where $id is an int that contains the thread number 
-# $data{$id}{'browser'}		where browser is a www::mechanize object that contains the webpage we are working with
-# $data{$id}{'cookie_jar'}	where cookie jar is a cookie store for the www:mechanize object
-# $data{$id}{'username'}	username
-# $data{$id}{'password'}	password
-# $data{$id}{'unpwindex'}	index of the in-use un/pw record in the %userlist hash
+# $data{'currentThreads'}	thread counter
 my %data;
+
+#data structure for threads
+# $thread_data{'browser'}		where browser is a www::mechanize object that contains the webpage we are working with
+# $thread_data{'cookie_jar'}	where cookie jar is a cookie store for the www:mechanize object
+# $thread_data{'username'}
+# $thread_data{'password'}
+my %thread_data;
+
 
 
 # where to point the script -change later
@@ -83,13 +96,9 @@ $target = 'http://sales.schoollogic.com/SchoolLogic/login.aspx?ReturnUrl=%2fscho
 
 
 
-my $maxThreads = 1;
-my $currentThreads = 0;
-my $threadInterval = 10;	#seconds
+my $maxThreads = 2;
+my $threadInterval = 0;	#seconds
 my $activityInterval = 10;
-my $newprocess;
-
-my $id = 0;
 
 
 while(1){
@@ -98,85 +107,57 @@ while(1){
 	sleep($threadInterval);
 
 	#make threads until we are at $maxThreads
-	if($currentThreads < $maxThreads){
+	if( threads->list(threads::running) < $maxThreads){
 
-		#increment the thread id counter
-		$id++;
 	
-		#initial timer for timing the thread
-		$data{$id}{'time'} = Time::HiRes::gettimeofday();
-
 		#child process time
-		$newprocess = fork();
-		if($newprocess == 0){
+		threads->create(sub {
+
+			#initial timer for timing the thread
+			$thread_data{'time'} = Time::HiRes::gettimeofday();
 		
-			#if we are here, we've created a new thread. increment the thread counter and log.
-			$currentThreads++;
-			log_message("$id, create, " . $data{$id}{'time'});
-			#how many threads are out there right now?
-			if($DEBUG){ print "current threads: $currentThreads , max threads: $maxThreads \n";}
+			log_message(threads->tid(). ", create, " . $thread_data{'time'});
 		
 			#create a new session. $data{$id}{browser} is the main object
 			#store cookie jar in memory in this threads hash
-			$data{$id}{'browser'} = WWW::Mechanize->new( cookie_jar => $data{$id}{'cookie_jar'} );
+			$thread_data{'browser'} = WWW::Mechanize->new( cookie_jar => $thread_data{'cookie_jar'} );
 			
 			#create a user-agent based on the thread id 
-			$data{$id}{'browser'}->agent($id);
+			$thread_data{'browser'}->agent(threads->tid());
 			
 			#we need to search through %userlist. this could create a race condition, so lock this function.
-			#this should be safe enough.
-			#loop until lock is free
-			USERLIST_INUSE_LOCK:
-			while($userlist_inuse_LOCK  != 0){
-				#wait;
-			}
-			if($userlist_inuse_LOCK == 0){
-				#if we are here that means this has become unlocked. so relock it.
-				$userlist_inuse_LOCK = 1;
-					
-				foreach my $userlist_sort_indexes (sort keys %userlist) {
-					#and find a user/pw combo that isnt in use
-					if($userlist{$userlist_sort_indexes}{'inuse'} == 0){
-						#then we are going to take this user and mark it in-use
-						$userlist{$userlist_sort_indexes}{'inuse'} = 1;
-						#and assign this thread to use this un/pw
-						$data{$id}{'username'} = $userlist{$userlist_sort_indexes}{'username'};
-						$data{$id}{'password'} = $userlist{$userlist_sort_indexes}{'password'};
-						$data{$id}{'unpwindex'} = $userlist_sort_indexes;
-					};
-				}
-				#we are done now, unlock.
-				$userlist_inuse_LOCK = 0;
-d			}
+			my $unpwindex = use_credentials();
 			
 			
+
+			
+				
 			
 			##LOGIN
 			#we make an initial request. login stage
 			#make request
-			$data{$id}{'browser'}->get( $target ); 
+#			$thread_data{'browser'}->get( $target ); 
 			
 			#check that things worked
-			if (! $data{$id}{'browser'}->success()) {
-				print $data{$id}{'browser'}->status() . "\n"; 
-			}
+#			if (! $thread_data{'browser'}->success()) {
+#				print $thread_data{'browser'}->status() . "\n"; 
+#			}
 			
-			#print $data{$id}{'browser'}->content();
-
+			
 			#now we login. the field() function sets the value of a field (www::mechanize magic). 
 			#populate username and password, hidden fields, then submit
-			$data{$id}{'result'} = $data{$id}{'browser'}->submit_form(	form_name => 'Form1',
-														fields => {
-															txtUserName => $data{$id}{'username'},
-															txtPassword => $data{$id}{'password'},
-															__EVENTVALIDATION => $data{$id}{'browser'}->value(__EVENTVALIDATION),
-															#__VIEWSTATE => $data{$id}{'browser'}->value(_VIEWSTATE),
-															Submit => 'Login',
-															},
-														button => 'Submit'
-														);
+#			$thread_data{'browser'}->submit_form(	form_name => 'Form1',
+#														fields => {
+#															txtUserName => $userlist{$unpwindex}{'username'},
+#															txtPassword => $userlist{$unpwindex}{'password'},
+#															__EVENTVALIDATION => $thread_data{'browser'}->value(__EVENTVALIDATION),
+#															#__VIEWSTATE => $thread_data{'browser'}->value(_VIEWSTATE),
+#															Submit => 'Login',
+#															},
+#														button => 'Submit'
+#														);
 			
-			#print $data{$id}{'result'};#->content();
+
 			
 
 			
@@ -186,8 +167,8 @@ d			}
 			#NEED TO TURN THIS SECTION IN FETCHING JOBS FROM THE WORK LIST
 			#{
 			#
-			#$data{$id}{'browser'}->get( 'https://schoollogic.psd70.ab.ca/Schoollogic/default.aspx' ); 
-			#print $data{$id}{'browser'}->content();
+			#$thread_data{'browser'}->get( 'https://schoollogic.psd70.ab.ca/Schoollogic/default.aspx' ); 
+			#print $thread_data{'browser'}->content();
 			#
 			#}
 			
@@ -203,44 +184,32 @@ d			}
 			
 
 		
-			#get time elapsed since thread started
-			$data{$id}{time} = Time::HiRes::gettimeofday() - $data{$id}{'time'};
+
+
 
 			
 			#check exit state of this thread
 			if($DEBUG){
-				#print Dumper \%data;
-				foreach $i (sort keys %data) {
-					foreach my $time (keys %{ $data{$id} }) {
-						#ignore the 'browser' element
-						#unless($time eq 'browser'){
-							print "$i: $time: $data{$id}{$time}\n";
-						#}
-					}
-				}
+				print threads->list(threads::running) . ", $maxThreads \n";
+				print threads->tid() . " un: " . $userlist{$unpwindex}{'username'} . "\n";
+				print threads->tid() . " pw: " . $userlist{$unpwindex}{'password'} . "\n";
+				print threads->tid() . " unpwindex: " . $unpwindex . "\n";
 			}				
 		
+			
+			#get time elapsed since thread started
+			$thread_data{'time'} = Time::HiRes::gettimeofday() - $thread_data{'time'};
 			#log and exit
-			log_message("$id, exit, " . $data{$id}{'time'});
+			log_message(threads->tid() . ", exit, " . $thread_data{'time'});
 
 			
 			
 			########################################THREAD CLEANUP
 			#release the username/password
-			$userlist{ $data{$id}{'unpwindex'} }{'inuse'} = 0;
-			$data{$id}{'username'} = '';
-			$data{$id}{'password'} = '';
-			$data{$id}{'unpwindex'} = '';
 			
-			sleep(1);
-			exit(0);
-		}
-
-
-		
-
-
-		
+			#exit thread	
+			threads->exit();
+		});		
 		
 		
 
@@ -269,14 +238,13 @@ d			}
 
 
 sub exit_clean(){
+	#put some thread cleanup in here
 	#close logfile if it exists
 	if($log_file != 0){ 
 		close($log_filehandle) || warn("could not close log file $log_file \n"); 
 	}
 	$SIG{'INT'}  = 'IGNORE';
 	$SIG{'QUIT'} = 'IGNORE';
-	close(SERVERSOCK);
-	close(CHLDSOCK);
 	die("Quitting on signal " . @_ . "\n");
 }
 
@@ -284,7 +252,6 @@ sub exit_clean(){
 sub child_handler(){
 	wait;
 }
-
 
 
 #sub that holds open the logfile so that we don't have overhead of opening and closing logfile for each message
@@ -309,6 +276,31 @@ sub log_message(){
 	}
 }
 
+#returns a free un/pw, and sets it in-use in the hash
+sub use_credentials() {
+	$returnVal = 0;
+	foreach my $userlist_sort_indexes (sort keys %userlist) {
+		next if $returnVal != 0;
+		#semaphore to lock this code block
+		$use_credentials_semaphore->down();
+		if($userlist{$userlist_sort_indexes}{'inuse'} == 0){
+		#then we are going to take this un/pw and mark it in-use
+			$userlist{$userlist_sort_indexes}{'inuse'} = 1;
+			#and return the index of this un/pw
+			$returnVal = $userlist_sort_indexes;
+		}
+		#semaphore to unlock this code block
+		$use_credentials_semaphore->up();
+	}
+	return $returnVal;
+}
+
+
+#frees a un/pw in use
+sub free_credentials() {
+	my $index = shift(@_);	
+	$userlist{$index}{'inuse'} = 0;
+}
 
 
 # for reference
